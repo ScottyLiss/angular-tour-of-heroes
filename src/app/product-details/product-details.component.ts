449e0c2afab0c240d230fd8221f7791ce7a698ab
Scott L <scottlissone@gmail.com>
2022-01-25 14:33:10 +0000

    Initial Submit

---
+++ src/app/product-details/product-details.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { Product, products } from '../products';
+import { CartService } from '../cart.service';
+
+// General way to handle imports, top is API's, below is custom API/Classes
+
+
+@Component({
+  selector: 'app-product-details',
+  templateUrl: './product-details.component.html',
+  styleUrls: ['./product-details.component.css']
+})
+export class ProductDetailsComponent implements OnInit {
+
+  product: Product | undefined;
+
+
+  constructor(
+    private route: ActivatedRoute,
+    private cartService: CartService
+    ) { }
+
+    addToCart(product: Product)
+    {
+      this.cartService.addToCart(product);
+      window.alert('Your product has been added to the cart!');
+    }
+
+  ngOnInit() 
+  {
+    // First get the product id from the current route
+    const routeParams = this.route.snapshot.paramMap;
+    const productIdFromRoute = Number(routeParams.get('productId'));
+    
+    // Find the product that correspons with the id provided in route.
+    this.product = products.find(product => product.id === productIdFromRoute);
+    
+  }
+
+}
